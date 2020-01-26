b272d6643ee6ddea63d6c873de7c80be44c27e24
Greg Marine <gregmarine@iccnet.org>
2020-01-25 20:48:23 -0500

    Updated the Firebase implementation

--- src/components/firebase/Firebase.ts
+++ src/components/firebase/Firebase.ts
@@ -1,1 +1,30 @@
+import app from 'firebase/app';
+import 'firebase/auth';
 import { config } from './firebaseConfig';
+
+class Firebase {
+    auth: any;
+    
+    constructor() {
+        app.initializeApp(config);
+
+        this.auth = app.auth();
+    }
+
+    doCreateUserWithEmailAndPassword = (email: string, password: string) =>
+        this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email: string, password: string) =>
+        this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () =>
+        this.auth.signOut();
+
+    doPasswordReset = (email: string) =>
+        this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = (password: string) =>
+        this.auth.currentUser.updatePassword(password);
+}
+
+export default Firebase;
